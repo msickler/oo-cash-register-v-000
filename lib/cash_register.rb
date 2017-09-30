11905b2280a26700fddd78b21558945d0ce8538b
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-30 18:01:50 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,11 +1,17 @@
 class CashRegister
 
-attr_accessor :total, :discount
+attr_accessor :total, :discount, :title, :price
 
   def initialize(discount=0)
     @total = 0
     @discount = discount
+  end
 
+  def add_item(title, price)
+    hash = {}
+    hash[title] = price
+    new_total = @total + price
+    new_total
   end
 
 end
